ae17e0fe2308606bc07ecef4f0ddec4b1e37b2ff
Hasan Sadique <hasansadiq1129@gmail.com>
2023-03-19 23:35:24 +0530

    CryptoApp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import store from './app/store';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,9 +15,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Router>
-      {/* <Provider store={store}> */}
-      <App />
-      {/* </Provider> */}
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
 );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,19 @@
+
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//         referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//         timePeriod: '24h',
+//         'tiers[0]': '1',
+//         orderBy: 'marketCap',
+//         orderDirection: 'desc',
+//         limit: '50',
+//         offset: '0'
+//     },
+//     headers: {
+//         'X-RapidAPI-Key': 'ba32e7762bmsh08d4abb6674400fp12c76cjsnf5e08e5b5a66',
+//         'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+// };
