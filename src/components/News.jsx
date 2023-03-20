f6e7100b7029ada2d2b83d8452d05d04005cae4c
Priyanshu <priyanshu.s1619@gmail.com>
2023-03-20 22:57:28 +0530

    Done

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,16 +4,15 @@ import moment from "moment";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const { Text, Title } = Typography
-const { Option } = Select
+const { Text, Title } = Typography;
+const { Option } = Select;
 const News = (simplified) => {
-    const { data, error, isLoading } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100 })
-    console.log(data)
-    return (
-        <div>
-Hello
-        </div>
-    )
-}
+  const { data, error, isLoading } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 10 : 100,
+  });
+  console.log(data);
+  return <div>Hello</div>;
+};
 
-export default News
+export default News;
